e50bbfbeb02e9b51ce6a095660e78f1b40684784
sfd-mhrana <sfd.mhrana@gmail.com>
2025-09-22 03:55:01 +0600

    change logger

--- LEADPRO_API/src/main.ts
+++ LEADPRO_API/src/main.ts
@@ -1,16 +1,13 @@
+import { envConfig } from '@flusys/flusysnest/core/config';
+import { instance } from '@flusys/flusysnest/shared/classes';
+import { TransformResponseInterceptor } from '@flusys/flusysnest/shared/interceptors';
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { Logger, VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+import { WinstonModule } from 'nest-winston';
 import { DataSource } from 'typeorm';
 import { AppModule } from './app.module';
-import { envConfig } from '@flusys/flusysnest/core/config';
-import { WinstonModule } from 'nest-winston';
-import { instance } from './winston.logger';
-import { TransformResponseInterceptor } from '@flusys/flusysnest/shared/interceptors';
-import { seedData } from '@flusys/flusysnest/core/data';
-import { seedMenuV1Data } from './persistence/seeds/menu_v1.seed';
-import { seedMenuV2Data } from './persistence/seeds/menu_v2.seed';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -31,7 +28,6 @@ async function bootstrap() {
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
   });
-
 
   app.useGlobalInterceptors(new TransformResponseInterceptor());
 
